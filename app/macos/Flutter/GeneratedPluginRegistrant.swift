29f4248cc7c1b719d644361f73714bc79cb1070d
DiegoM <merinogomez17@gmail.com>
2026-08-16 22:43:05 -0500

    Cambia configuracion de IOS local con lo subido desde Windows

--- app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,6 @@ import app_links
 import audio_session
 import google_sign_in_ios
 import just_audio
-import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
 import video_player_avfoundation
@@ -19,8 +18,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+  VideoPlayerPlugin.register(with: registry.registrar(forPlugin: "VideoPlayerPlugin"))
 }
